e65ccdd9e3e756714bd0763b3ac6fa81bb35573d
goma6686 <goma6686@mif.stud.vu.lt>
2022-04-07 22:01:43 +0300

    Added main page and three rooms. Working

--- index.js
+++ index.js
@@ -7,20 +7,39 @@ server.listen(port, () => {
     console.log(`server running on port ${port}`);
 });
 
+//ROUTES
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html') //pass index.html
 });
+app.get('/judesys', (req, res) => {
+    res.sendFile(__dirname + '/public/judesys.html') //pass judesys.html
+});
+app.get('/sunflower', (req, res) => {
+    res.sendFile(__dirname + '/public/sunflower.html') //pass sunflower.html
+});
+app.get('/plazmoidas', (req, res) => {
+    res.sendFile(__dirname + '/public/plazmoidas.html') //pass plazmoidas.html
+});
 
-io.on('connection', (socket) => {
+//namespace !
+const tech = io.of('/tech');
+
+tech.on('connection', (socket) => {
     console.log('someone connected!');
-    socket.on('message', (msg) => {
-        console.log(`message: ${msg}`);
-        io.emit('message', msg);
+
+    socket.on('join', (data) => {  //join event is from documentation
+        socket.join(data.room);
+        tech.in(data.room).emit('message', `New user has joined ${data.room}!`) //when someone joins
+    });
+
+    socket.on('message', (data) => {
+        console.log(`message: ${data.msg}`);
+        tech.in(data.room).emit('message', data.msg);  //when someone says something emit to the room
     });
 
     socket.on('disconnected', () => {
         console.log('user has left!');  // <-- to console
 
-        io.emit('message', 'user has left!'); // <-- to everyone
+        tech.emit('message', 'user has left!'); // <-- to everyone
     });
 });
